97ec1ac516d313f4dfc475056aed438e781a1420
Norbert Bodziony <norbertbodziony@gmail.com>
2021-03-07 13:05:59 +0100

    create password page

---
+++ src/components/CreatePassword/CreatePassword.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import CreatePassword from './CreatePassword'
+import { withKnobs } from '@storybook/addon-knobs'
+import { withBackground } from '../../../.storybook/decorators'
+
+storiesOf('Pages/CreatePassword', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withBackground)
+  .add('default', () => {
+    const [password, setPassword] = React.useState('')
+    return (
+      <div>
+        <CreatePassword
+          password={password}
+          setPassword={setPassword}
+          onClick={() => {
+            console.log('click')
+          }}
+        />
+      </div>
+    )
+  })
--- src/components/CreatePassword/CreatePassword.tsx
+++ src/components/CreatePassword/CreatePassword.tsx
@@ -1,14 +1,8 @@
 import React, { useState } from 'react'
-import InputLabel from '@material-ui/core/InputLabel'
-import InputAdornment from '@material-ui/core/InputAdornment'
-import FormControl from '@material-ui/core/FormControl'
-import Visibility from '@material-ui/icons/Visibility'
-import VisibilityOff from '@material-ui/icons/VisibilityOff'
-import Input from '@material-ui/core/Input'
-import IconButton from '@material-ui/core/IconButton'
-import CommonButton from '@components/CommonButton/CommonButton'
-import { Grid, OutlinedInput, Typography } from '@material-ui/core'
+import { Grid, Typography } from '@material-ui/core'
 import useStyles from './style'
+import PasswordInput from '@components/PasswordInput/PasswordInput'
+import FilledButton from '@components/FilledButton/FilledButton'
 interface IProps {
   onClick: () => void
   setPassword: (password: string) => void
@@ -16,80 +10,40 @@ interface IProps {
 }
 export const CreatePassword: React.FC<IProps> = ({ onClick, password, setPassword }) => {
   const classes = useStyles()
-  const [show, setShow] = useState(false)
-  const [show2, setShow2] = useState(false)
   const [passwordConfirm, setPasswordConfirm] = useState('')
   return (
-    <Grid container direction='column' alignItems='center' justify='center'>
-      <Grid item>
-        <Typography variant='h3'>Create Password</Typography>
+    <Grid
+      container
+      direction='column'
+      alignItems='center'
+      justify='center'
+      className={classes.root}>
+      <Grid item style={{ marginTop: 100 }}>
+        <Typography variant='body2' className={classes.welcome}>
+          Welcome
+        </Typography>
+      </Grid>
+      <Grid item style={{ marginTop: 15 }}>
+        <Typography variant='h1' className={classes.title}>
+          Create Password
+        </Typography>
       </Grid>
       <Grid item style={{ marginTop: 30 }}>
-        <FormControl variant='outlined'>
-          <InputLabel htmlFor='outlined-adornment-password' className={classes.label}>
-            Password
-          </InputLabel>
-          <OutlinedInput
-            className={classes.input}
-            color='primary'
-            id='outlined-adornment-password'
-            type={show ? 'text' : 'password'}
-            value={password}
-            onChange={e => {
-              setPassword(e.target.value)
-            }}
-            endAdornment={
-              <InputAdornment position='end'>
-                <IconButton
-                  className={classes.showButton}
-                  aria-label='toggle password visibility'
-                  onClick={() => {
-                    setShow(!show)
-                  }}
-                  edge='end'>
-                  {show ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-            labelWidth={85}
-          />
-        </FormControl>
+        <PasswordInput
+          password={password}
+          setPassword={setPassword}
+          label='Password*'></PasswordInput>
       </Grid>
       <Grid item style={{ marginTop: 30 }}>
-        <FormControl variant='outlined'>
-          <InputLabel htmlFor='outlined-adornment-password' className={classes.label}>
-            Confirm Password
-          </InputLabel>
-          <OutlinedInput
-            className={classes.input}
-            color='primary'
-            id='outlined-adornment-password'
-            type={show2 ? 'text' : 'password'}
-            value={passwordConfirm}
-            onChange={e => {
-              setPasswordConfirm(e.target.value)
-            }}
-            endAdornment={
-              <InputAdornment position='end'>
-                <IconButton
-                  className={classes.showButton}
-                  aria-label='toggle password visibility'
-                  onClick={() => {
-                    setShow2(!show2)
-                  }}
-                  edge='end'>
-                  {show2 ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-            labelWidth={165}
-          />
-        </FormControl>
+        <PasswordInput
+          password={passwordConfirm}
+          setPassword={setPasswordConfirm}
+          label='Repeat password*'></PasswordInput>
       </Grid>
       <Grid item style={{ marginTop: 30 }}>
-        <CommonButton
-          name='confirm'
-          disabled={password.length < 2 || password != passwordConfirm}
+        <FilledButton
+          name='Create account'
+          disabled={password.length < 2 || password !== passwordConfirm}
           onClick={() => {
             onClick()
           }}
